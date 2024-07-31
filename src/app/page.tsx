7d52facaa1666dcf8127eb0350a7832cd5b904b8
Oliveira-Renato <renato.pritf@gmail.com>
2024-07-31 14:37:23 -0300

    section: main done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import mainImage from "/public/images/main/m-main-3.jpg";
+import mainAboutImage from "/public/images/about/m-about.jpg";
+import mainAboutImage2 from "/public/images/about/m-about-3.jpg";
 import bgTeste from "/public/bg-teste.png";
 
 export default function Home() {
@@ -8,61 +10,103 @@ export default function Home() {
       {/* Main Section */}
       <section
         id="main"
-        className="h-screen flex flex-col md:flex-row items-center justify-center bg-cover bg-center p-4 overflow-hidden  relative"
+        className="h-screen grid grid-cols-2 bg-cover bg-center px-20 overflow-hidden relative bg-gray-200"
       >
-        <aside className="w-full md:w-1/2 h-64 md:h-auto mb-8 md:mb-0 md:mr-8">
+        <aside className="w-full md:h-auto mb-8 md:mb-0 md:mr-8">
           <div>
             <Image
               src={mainImage}
               width={500}
               height={500}
               alt="Foto principal da modelo"
-              className="object-cover max-h-fit border-4 border-red-500 neon-border saturate-red brightness-150 saturate-125"
+              className="object-cover max-h-fit polaroid saturate-red brightness-150 saturate-125 max-w-full"
             />
           </div>
         </aside>
-        <div className="text-center md:text-left">
-          <h1 className="text-6xl font-bold text-textMain leading-tight tracking-wider font-primary">
-            Mary Mendes
-          </h1>
-          <p className="text-2xl text-textSecondary mt-4 max-w-lg leading-relaxed">
-            <span>Modelo</span> e <span>criadora de conteúdo</span>, apaixonada
-            por moda, desfiles, edição de vídeos e música.
-          </p>
+
+        <div className="h-screen text-center flex items-center justify-center md:text-left">
+          <div className="">
+            <h1 className="text-6xl font-bold text-textMain leading-tight tracking-wider font-primary">
+              Mary Mendes
+            </h1>
+            <p className="text-2xl text-textSecondary mt-4 max-w-lg leading-relaxed">
+              <span>Modelo</span> e <span>criadora de conteúdo</span>,
+              apaixonada por moda, desfiles, edição de vídeos e música.
+            </p>
+          </div>
         </div>
-        <Image
-          src={bgTeste}
-          width={500}
-          height={500}
-          alt="Bg"
-          className="object-cover w-full h-ful absolute"
-        />
+
+        <div className="w-full h-full absolute grayscale">
+          <Image
+            src={bgTeste}
+            width={500}
+            height={500}
+            alt="Bg"
+            className="object-cover w-full h-full"
+          />
+        </div>
       </section>
 
       {/* About Section */}
       <section
         id="about"
-        className="h-screen flex items-center justify-center bg-cover bg-center py-16 bg-gray-100"
+        className="h-screen grid grid-cols-1 md:grid-cols-3 bg-gray-100 overflow-hidden"
       >
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl font-bold mb-8">Sobre Mim</h2>
-          <p className="text-lg mb-6">
-            Mary Mendes é uma modelo profissional com vasta experiência em
-            campanhas publicitárias, desfiles de moda e fotos editoriais. Com um
-            estilo único e presença marcante, Mary encanta em cada trabalho que
-            realiza.
-          </p>
-          <p className="text-lg">
-            Fora das câmeras, Mary gosta de viajar, praticar yoga e se dedicar à
-            filantropia.
-          </p>
+        <div className="flex justify-end items-center ml-20 px-4 col-span-1 w-full">
+          <div className="text-center md:text-left">
+            <h2 className="text-6xl font-bold text-textMain leading-tight tracking-wider font-primary">
+              Sobre Mim
+            </h2>
+            <p className="text-lg mb-6 text-2xl text-textSecondary mt-4 max-w-lg leading-relaxed">
+              Mary Mendes é uma modelo profissional com vasta experiência em
+              campanhas publicitárias, desfiles de moda e fotos editoriais. Com
+              um estilo único e presença marcante, Mary encanta em cada trabalho
+              que realiza.
+            </p>
+            <p className="text-lg text-2xl text-textSecondary mt-4 max-w-lg leading-relaxed">
+              Fora das câmeras, Mary gosta de viajar, praticar yoga e se dedicar
+              à filantropia.
+            </p>
+          </div>
         </div>
+
+        <div className="h-full flex justify-end items-center">
+          <div className="relative polaroid w-2/3 h-2/3">
+            <Image
+              src={mainAboutImage2}
+              width={300}
+              height={300}
+              alt="Foto Sobre da modelo"
+              className="object-cover"
+            />
+          </div>
+        </div>
+
+        <aside className="flex items-center justify-center">
+          <div className="w-full h-full">
+            <Image
+              src={mainAboutImage}
+              width={500}
+              height={500}
+              alt="Foto Sobre da modelo"
+              className="object-cover"
+            />
+          </div>
+        </aside>
+
+        <Image
+          src={bgTeste}
+          width={500}
+          height={500}
+          alt="Bg"
+          className="object-cover w-full h-full absolute grayscale"
+        />
       </section>
 
       {/* Portfolio Section */}
       <section
         id="portfolio"
-        className="h-screen flex items-center justify-center bg-cover bg-center py-16"
+        className="h-screen flex items-center justify-center bg-cover bg-center py-16 relative"
       >
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-8">Portfólio</h2>
